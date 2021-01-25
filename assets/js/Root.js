67bee18a1298e9a526c5203a3cb6b1574c5cd088
Nicholas Brown <countdooku.steak@gmail.com>
2021-01-24 16:38:21 -0800

    Initial commit

---
+++ assets/js/Root.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default class Root extends React.Component {
+  render() {
+    return (
+      <p>Welcome to Lutis!</p>
+    )
+  }
+}
---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+import "../css/app.scss"
+
+import "phoenix_html"
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+import Root from "./Root"
+
+ReactDOM.render(<Root />, document.getElementById("root"));
